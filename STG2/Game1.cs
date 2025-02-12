014ba239228508de9612490c91211523cc25b580
song <yuqun.song@wsu.edu>
2025-02-12 04:07:09 -0800

    player

---
+++ STG2/Game1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SharpDX.Direct3D9;
+using STG;
+
+namespace STG2
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Texture2D _texture;
+        private Texture2D _texture2;
+        private Texture2D _texture3;
+
+        private Background _background;
+        private PlayerPlanne _playerPlanne;
+        private Menu _menu;
+        private bool _gameStarted = false;
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferHeight = 850;
+            _graphics.PreferredBackBufferWidth = 480;
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+           
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _texture = Content.Load<Texture2D>("background3");
+            _texture2 = Content.Load<Texture2D>("plane");
+            _background = new Background( new Vector2(0, 0),4, _texture);
+            _playerPlanne = new PlayerPlanne(new Vector2(200, 750), _texture2,10,5,STG.direction.Up);
+            _texture3 = Content.Load<Texture2D>("button2");
+            var font = Content.Load<SpriteFont>("Fonts");
+            _menu = new Menu(_texture3, font, new Vector2(100, 400),"Start");
+           
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            // TODO: Add your update logic here
+            if (!_gameStarted) {
+                _menu.Update(gameTime);
+                if (_menu.IsPressed) {
+                    _gameStarted = true;
+                }
+            }
+            else {
+                var keyboardState = Keyboard.GetState();
+                if (keyboardState.IsKeyDown(Keys.W))
+                {
+                    _playerPlanne.Direction = direction.Up;
+                }
+                else if (keyboardState.IsKeyDown(Keys.S))
+                {
+                    _playerPlanne.Direction = direction.Down;
+                }
+                else if (keyboardState.IsKeyDown(Keys.A))
+                {
+                    _playerPlanne.Direction = direction.Left;
+                }
+                else if (keyboardState.IsKeyDown(Keys.D))
+                {
+                    _playerPlanne.Direction = direction.Right;
+                }
+                _playerPlanne.Update();
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+
+            // TODO: Add your drawing code here
+            GraphicsDevice.Clear(Color.Transparent);
+
+            _spriteBatch.Begin();
+            _background.Draw(_spriteBatch);
+            if (!_gameStarted) { 
+                _menu.Draw(_spriteBatch);
+            }
+            else {
+                _playerPlanne.Draw(_spriteBatch);
+            }
+               
+       
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ STG2/Menu.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SharpDX.Direct3D9;
+using STG;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace STG2
+{
+    public class Menu{
+
+        private MouseState _currentMouse;
+        private SpriteFont _font;
+        private string text;
+        private Texture2D _texture;
+        private MouseState _previousMouse;
+
+        public Vector2 Position { get; set; }
+    
+        public bool IsPressed { get; private set; }
+
+
+        public string Text { get; set; }
+
+        public Menu(Texture2D texture, SpriteFont font, Vector2 poistion, string text)
+        {
+            _texture = texture;
+            _font = font;
+            Position = poistion;
+            Text = text;
+        }
+
+        public void Update(GameTime gameTime){
+           _previousMouse = _currentMouse;
+           _currentMouse = Mouse.GetState();
+           if (new Rectangle((int)Position.X,(int)Position.Y,_texture.Width,_texture.Height).Contains(_currentMouse.X, _currentMouse.Y))
+            {
+                if (_currentMouse.LeftButton == ButtonState.Pressed &&_previousMouse.LeftButton == ButtonState.Released)
+                {
+                    IsPressed = true;
+                }        
+            }
+         }
+       
+    
+
+         public void Draw(SpriteBatch spriteBatch){
+
+            spriteBatch.Draw(_texture, new Rectangle((int)Position.X, (int)Position.Y, _texture.Width, _texture.Height), Color.White);
+
+            Vector2 textSize = _font.MeasureString(Text);
+            Vector2 buttonCenter = new Vector2(
+                Position.X + _texture.Width / 2f,
+                Position.Y + _texture.Height / 2f
+            );
+            Vector2 textPosition = buttonCenter - textSize / 2f;
+
+            spriteBatch.DrawString(_font, Text, textPosition, Color.White);
+        }
+    }
+}
