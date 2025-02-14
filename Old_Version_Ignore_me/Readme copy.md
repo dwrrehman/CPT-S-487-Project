176daac0a1583dbb39b48b21bda11eb77a4a6675
Daniel Rehman <dwrrehman@gmail.com>
2025-02-13 19:22:33 -0800

    Merge the state of the player branch into the main branch.

---
+++ Old_Version_Ignore_me/Readme copy.md
@@ -0,0 +1,5 @@
+# CPT-S-487-Project
+
+Note: Currently on Deliverable 1. This includes player movement and basic window functionality, as well as enemies. 
+
+
---
+++ STG2/PlayerPlanne.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using STG;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STG2
+{
+    class PlayerPlanne : Character
+    {
+        private Texture2D _texture;
+
+        public PlayerPlanne(Vector2 position,Texture2D texture, int health, int speed, direction direction) : base(position,texture.Width,texture.Height, health, speed,direction)
+        {
+            _texture = texture;
+
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture: _texture, destinationRectangle: new Rectangle((int)this.Position.X, (int)this.Position.Y, 100,100), color: Color.White);
+
+        }
+        public void Update()
+        {
+            this.Move();
+        }
+
+    }
+}
